64330bce053dc4cc00da2ce0236f8098fc1ac252
Lio_83 <l.b.2019.var@gmail.com>
2022-04-14 00:56:31 +0200

    init

--- main.lua
+++ main.lua
@@ -6,8 +6,7 @@ io.stdout:setvbuf('no')
 love.graphics.setDefaultFilter("nearest")
 
 function love.load()
-  local test = 10;
-  local test2 = 23;
+
   largeur_ecran = love.graphics.getWidth()
   hauteur_ecran = love.graphics.getHeight()
   
