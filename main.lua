3f20a687e139cde3b9eeca5d09f9736c1c92ea2d
Lio_83 <l.b.2019.var@gmail.com>
2022-04-14 00:54:03 +0200

    nouvelle modification

--- main.lua
+++ main.lua
@@ -6,7 +6,8 @@ io.stdout:setvbuf('no')
 love.graphics.setDefaultFilter("nearest")
 
 function love.load()
-  
+  local test = 10;
+  local test2 = 23;
   largeur_ecran = love.graphics.getWidth()
   hauteur_ecran = love.graphics.getHeight()
   
