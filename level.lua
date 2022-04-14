16b1f1586e881bc1dad20ee12300a93c31f0bb28
Lio_83 <l.b.2019.var@gmail.com>
2022-04-14 22:38:21 +0200

    init

--- level.lua
+++ level.lua
@@ -3,6 +3,9 @@ local level = {}
 function level.Load()
 end
 
+function level.Update(dt)
+end
+
 function level.Draw()
 end
 
--- main.lua
+++ main.lua
@@ -38,5 +38,4 @@ function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end   
-
 end
